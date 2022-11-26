75cbe43e186522a8c389cb1223de6066d5c7febd
datntph24772 <117461887+datntph24772@users.noreply.github.com>
2022-11-26 16:58:18 +0700

    dat code crud user

--- admin/models/m_user.php
+++ admin/models/m_user.php
@@ -11,12 +11,12 @@ class m_user extends database {
         $this->setQuery($sql);
         return $this->execute(array($id));
     }
-    public function insertuser($username, $password, $email,$id_role)
+    public function insertuser($username, $password, $email,$chose)
     {
-        $sql = "insert into users(username,password,email,time,id_role)
-                values ('$username','$password','$email',?,'$id_role')";
+        $sql = "insert into users(username,password,email,id_role)
+                values (?,?,?,?)";
         $this->setQuery($sql);
-        return $this->execute(array($username, $password, $email,$id_role));
+        return $this->execute(array($username, $password, $email,$chose));
     }
     public function showUser()
     {
@@ -25,18 +25,18 @@ class m_user extends database {
         return $this->loadAllRows();
     }
     public function read_user_by_id ($id) {
-        $sql = "select * from users where id_user = ?";
+        $sql = "select username, email, id_role from users where id_user = ?";
         $this->setQuery($sql);
         return $this->loadRow(array($id));
     }
-    public function update_user_by_id($fullname, $username,$email,$id)
+    public function update_user_by_id($username,$email,$chose,$id)
     {
         // $sql = "UPDATE `products` SET `name_product`='$ten_sp', `image`='$hinh',` price` ='$gia', `description` ='$mota', `cate_id`='$loai_sp',
         // where id = '$id'";
-        $sql = "update users set fullname = ?,username = ?,password = ?,email = ?,date_registration=?, id_role=? where
+        $sql = "update users set username = ?,email = ?, id_role=? where
         id_user = ?";
         $this->setQuery($sql);
-        return $this->execute(array($fullname,$username,$email,$id));
+        return $this->execute(array($username,$email,$chose,$id));
     
     }
     
